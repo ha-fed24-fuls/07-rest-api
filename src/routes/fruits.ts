ac628d370c0ea42cb54975c427bd4cbebebbe7dd
David Andersson <lejonmanen@gmail.com>
2025-09-03 13:56:39 +0200

    GET /fruits/:id

--- src/routes/fruits.ts
+++ src/routes/fruits.ts
@@ -1,5 +1,5 @@
 import express from 'express'
-import type { Router } from 'express'
+import type { Request, Response, Router } from 'express'
 
 const router: Router = express.Router()
 
@@ -8,9 +8,27 @@ interface Fruit {
 	name: string;
 	price: number;
 }
-const data: Fruit[] = []
+const data: Fruit[] = [
+	{ id: 'q1', name: 'purjolök', price: 12 },
+	{ id: 'q2', name: 'banan', price: 77 },
+	{ id: 'q3', name: 'melon', price: 20 },
+]
 router.get('/', (req, res) => {
 	res.send(data)
+})
+interface Params {
+	id: string;
+}
+router.get('/:id', (req: Request<Params>, res: Response<Fruit>) => {
+	const id: string = req.params.id
+	const maybeFruit: Fruit | undefined = data.find(fruit => fruit.id === id)
+	// Två möjligheter: antingen finns frukten med "id" eller inte
+	if( maybeFruit ) {
+		res.send(maybeFruit)   // status 200 OK
+
+	} else {
+		res.sendStatus(404)  // 404 Not Found
+	}
 })
 
 export default router
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 import express from 'express'
-import type { Express } from 'express'
+import type { Express, RequestHandler } from 'express'
 import fruitsRouter from './routes/fruits.js'
 
 const app: Express = express()
 const port = 1337  // byt portnummer om den är upptagen på din dator
 
 
+const logger: RequestHandler = (req, res, next) => {
+	console.log(`${req.method}  ${req.url}`, req.body)
+	next()
+}
+
+app.use('/', logger)
 app.use('/fruits', fruitsRouter)
 
 
